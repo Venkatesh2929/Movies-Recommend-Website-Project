49d8d596ea79b8c5f37007388d1e7d71816dd59a
rnhvenkatesh <reddynagaharavenkatesh@gmail.com>
2024-06-11 10:03:02 +0530

    modified:   Backend Development/__pycache__/admin_login.cpython-312.pyc 	modified:   Backend Development/main.py 	new file:   templates/Frontend Development/add_movie.html 	modified:   templates/Frontend Development/admin_dashboard.html 	modified:   templates/Frontend Development/catalog.html 	new file:   templates/Frontend Development/edit_movie.html 	modified:   templates/Frontend Development/login.html

--- Backend Development/main.py
+++ Backend Development/main.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 from user_login import get_logged_in_user, register_user
+from admin_login import admin_login  # Import the admin_login function
 from movies import movies_bp, get_movies  # Import the movies Blueprint and get_movies function
 import mysql.connector
 
 app = Flask(__name__, template_folder='C:\\Users\\reddy\\Movies Recommend Website Project\\templates\\Frontend Development', static_folder='C:\\Users\\reddy\\Movies Recommend Website Project\\templates\\static')
 
 # Database connection
-mydb = mysql.connector.connect(
-        host="127.0.0.3",
-        user="root",
-        password="123456789",
-        database="MovieDB"
-)
+db_config = {
+    'host': '127.0.0.3',
+    'user': 'root',
+    'password': '123456789',
+    'database': 'MovieDB'
+}
+mydb = mysql.connector.connect(**db_config)
 
 # Register the movies Blueprint
 app.register_blueprint(movies_bp, url_prefix='/movies')
@@ -37,7 +39,9 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if get_logged_in_user(username, password):
+        if admin_login(username, password):
+            return redirect(url_for('admin_dashboard'))
+        elif get_logged_in_user(username, password):
             return redirect(url_for('welcome', username=username))
         else:
             return render_template('login.html', error='Invalid username or password')
@@ -67,7 +71,7 @@ def get_movies(category, sort_by='release_date'):
         sort_column = 'release_date'  # Default sorting
     
     query = f"""
-        SELECT movie_name, cast, summary, release_date, ratings, ott_link, ott_platform 
+        SELECT movie_name, cast, summary, release_date, ratings, ott_link, ott_platform, category
         FROM {category.lower()}_movies 
         ORDER BY {sort_column} DESC
     """
@@ -80,6 +84,67 @@ def catalog(category):
     sort_by = request.args.get('sort_by', 'release_date')
     movies = get_movies(category, sort_by)
     return render_template('catalog.html', category=category, movies=movies)
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    cursor = mydb.cursor()
+    categories = ['Telugu', 'Hindi', 'English']
+    movies_by_category = {category: get_movies(category) for category in categories}
+    return render_template('admin_dashboard.html', movies_by_category=movies_by_category)
+
+@app.route('/add_movie', methods=['GET', 'POST'])
+def add_movie():
+    if request.method == 'POST':
+        category = request.form['category']
+        movie_name = request.form['movie_name']
+        cast = request.form['cast']
+        summary = request.form['summary']
+        release_date = request.form['release_date']
+        ratings = request.form['ratings']
+        ott_link = request.form['ott_link']
+        ott_platform = request.form['ott_platform']
+        cursor = mydb.cursor()
+        query = f"""
+            INSERT INTO {category.lower()}_movies 
+            (movie_name, cast, summary, release_date, ratings, ott_link, ott_platform, category)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(query, (movie_name, cast, summary, release_date, ratings, ott_link, ott_platform, category))
+        mydb.commit()
+        return redirect(url_for('admin_dashboard'))
+    return render_template('add_movie.html')
+
+@app.route('/edit_movie/<category>/<movie_name>', methods=['GET', 'POST'])
+def edit_movie(category, movie_name):
+    cursor = mydb.cursor()
+    if request.method == 'POST':
+        new_movie_name = request.form['movie_name']
+        cast = request.form['cast']
+        summary = request.form['summary']
+        release_date = request.form['release_date']
+        ratings = request.form['ratings']
+        ott_link = request.form['ott_link']
+        ott_platform = request.form['ott_platform']
+        query = f"""
+            UPDATE {category.lower()}_movies 
+            SET movie_name=%s, cast=%s, summary=%s, release_date=%s, ratings=%s, ott_link=%s, ott_platform=%s 
+            WHERE movie_name=%s
+        """
+        cursor.execute(query, (new_movie_name, cast, summary, release_date, ratings, ott_link, ott_platform, movie_name))
+        mydb.commit()
+        return redirect(url_for('admin_dashboard'))
+    query = f"SELECT * FROM {category.lower()}_movies WHERE movie_name=%s"
+    cursor.execute(query, (movie_name,))
+    movie = cursor.fetchone()
+    return render_template('edit_movie.html', movie=movie)
+
+@app.route('/delete_movie/<category>/<movie_name>')
+def delete_movie(category, movie_name):
+    cursor = mydb.cursor()
+    query = f"DELETE FROM {category.lower()}_movies WHERE movie_name=%s"
+    cursor.execute(query, (movie_name,))
+    mydb.commit()
+    return redirect(url_for('admin_dashboard'))
 
 if __name__ == '__main__':
     app.run(debug=True)
